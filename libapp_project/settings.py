c400169a621e0d813e2a9fb699e85ef84cc4a633
Boulevard <30576543+BeachBoulevard@users.noreply.github.com>
2020-04-15 15:56:15 +0200

    static_assets

--- libapp_project/settings.py
+++ libapp_project/settings.py
@@ -38,10 +38,15 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+
+    #Third party
+    'crispy_forms',
+
     #local
     'users.apps.UsersConfig',
     'pages.apps.PagesConfig',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -126,5 +131,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_FINDERS = [
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
+
 
 AUTH_USER_MODEL = 'users.CustomUser'
+
+#django-crispy-forms
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
