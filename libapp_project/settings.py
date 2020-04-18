8674b77105317de1e5ecae6f9df8ee4b7be1791e
Boulevard <30576543+BeachBoulevard@users.noreply.github.com>
2020-04-18 19:26:16 +0200

    environment variables

--- libapp_project/settings.py
+++ libapp_project/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*3dm7$o83qo=)(m=7qg113)1^jxs!n%-9$4i5k7m^20%89#=9y'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', default=0))
 
 ALLOWED_HOSTS = []
 
