849a8603c782d95c9a94a31a90b07c8d03ea546c
Boulevard <30576543+BeachBoulevard@users.noreply.github.com>
2020-04-20 00:06:17 +0200

    homepage via pages app creation

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView
+from .views import HomePageView, AboutPageView, BycategoryPageView
 
 urlpatterns = [
   path('about/', AboutPageView.as_view(), name='about'),
+  path('bycategory/', BycategoryPageView.as_view(), name='bycategory'),
   path('', HomePageView.as_view(), name='home'),
 
 ]
--- pages/views.py
+++ pages/views.py
@@ -8,3 +8,6 @@ class HomePageView(TemplateView):
 
 class AboutPageView(TemplateView):
   template_name = 'about.html'
+
+class BycategoryPageView(TemplateView):
+  template_name = 'bycategory.html'
